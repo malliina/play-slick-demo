ebb28893092b2f9da789d76e9b9d75937878d161
malliina <malliina123@gmail.com>
2015-07-13 13:54:48 +0300

    Initial commit

---
+++ project/PlayBuild.scala
@@ -0,0 +1,34 @@
+import com.mle.sbtplay.PlayProjects
+import sbt.Keys._
+import sbt._
+
+object PlayBuild extends Build {
+  lazy val p = PlayProjects.plainPlayProject("play-slick-demo").settings(commonSettings: _*)
+
+  lazy val commonSettings = Seq(
+    organization := "com.github.malliina",
+    version := "0.0.1",
+    scalaVersion := "2.11.6",
+    retrieveManaged := false,
+    fork in Test := true,
+    updateOptions := updateOptions.value.withCachedResolution(true),
+    libraryDependencies ++= Seq(
+      "com.github.malliina" %% "play-base" % "0.5.0"),
+    exportJars := true,
+    javacOptions ++= Seq("-source", "1.8", "-target", "1.8"),
+    scalacOptions ++= Seq(
+      "-target:jvm-1.8",
+      "-deprecation",
+      "-encoding", "UTF-8",
+      "-unchecked",
+      "-feature",
+      "-language:existentials",
+      "-language:higherKinds",
+      "-language:implicitConversions",
+      "-Xfatal-warnings",
+      "-Xlint",
+      "-Yno-adapted-args",
+      "-Ywarn-dead-code",
+      "-Ywarn-numeric-widen")
+  )
+}
---
+++ project/project/BuildBuild.scala
@@ -0,0 +1,25 @@
+import sbt._
+import sbt.Keys._
+
+/**
+ *
+ * @author mle
+ */
+object BuildBuild extends Build {
+  // "build.sbt" goes here
+  override lazy val settings = super.settings ++ Seq(
+    scalaVersion := "2.10.4",
+    resolvers ++= Seq(
+      Resolver.url("malliina bintray sbt", url("https://dl.bintray.com/malliina/sbt-plugins/"))(Resolver.ivyStylePatterns)
+    ),
+    scalacOptions ++= Seq("-unchecked", "-deprecation")
+  ) ++ sbtPlugins
+
+  def sbtPlugins = Seq(
+    "com.typesafe.play" % "sbt-plugin" % "2.4.0",
+    "com.github.malliina" % "sbt-play" % "0.3.1"
+  ) map addSbtPlugin
+
+  override lazy val projects = Seq(root)
+  lazy val root = Project("plugins", file("."))
+}
